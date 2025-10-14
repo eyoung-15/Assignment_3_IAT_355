89d40204b25490fd95ac81db07ba1978a74acf27
Emily Youngward <eyoungwa@gmail.com>
2025-10-14 16:37:39 -0700

    updating visualizations

--- javascript/visualizations.js
+++ javascript/visualizations.js
@@ -8,27 +8,63 @@ fetchData().then(async (data) => {
         .markBar()
         .data(data)
         .encode(
+            vl.x().fieldQ("Global_Sales").aggregate("sum").title("Total Global Sales (in millions)"),
             vl.y().fieldN("Platform").sort("-x"),
-            vl.x().fieldQ("Global_Sales").aggregate("sum")
+            vl.color().fieldN("Genre").title("Genre"),
+            vl.tooltip([
+                vl.fieldN("Genre"),
+                vl.fieldN("Platform")
+            ])
         )
+
         .width("container")
         .height(400)
         .toSpec();
 
     const vlSpec2 = vl
-        .markBar()
+        .markCircle()
         .data(data)
         .encode(
-            vl.y().fieldN("Genre").sort("-x"),
-            vl.x().fieldQ("Global_Sales").aggregate("sum"),
-            vl.color().value("teal")
+            vl.x().fieldO("Year").title("Year"),
+            vl.y().fieldQ("Global_Sales").aggregate("sum").title("Total Global Sales (in millions)"),
+            vl.text().fieldN("Genre").title("Genre"),
+            vl.color().fieldN("Platform").title("Platform"),
+            vl.tooltip([
+                vl.fieldO("Year"),
+                vl.fieldN("Genre"),
+                vl.fieldN("Platform"),
+                vl.fieldQ("Global_Sales")
+
+            ])
         )
         .width("container")
         .height(400)
         .toSpec();
 
+    const vlSpec3 = vl
+        .markBar()
+        .data(data)
+        .encode(
+            vl.x().fieldO("Year").title("Year"),
+            vl.y().fieldQ("Global_Sales").aggregate("sum").title("Total Global Sales (in millions)"),
+            vl.text().fieldN("Genre").title("Genre"),
+            vl.color().fieldN("Platform").title("Platform"),
+            vl.tooltip([
+                vl.fieldO("Year"),
+                vl.fieldN("Genre"),
+                vl.fieldN("Platform"),
+                vl.fieldQ("Global_Sales")
+
+            ])
+        )
+        .width("container")
+        .height(400)
+        .toSpec();
+
+
     render("#view", vlSpec);
     render("#view2", vlSpec2);
+    render("#view3", vlSpec3);
 });
 
 async function render(viewID, spec) {
